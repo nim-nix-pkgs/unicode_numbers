2f60a1ba483b6c7c36e5d2560906020e5236bd63
hugosenari <hugosenari@gmail.com>
2022-05-09 20:44:57 -0300

    chore: re index unicode_numbers

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."unicode_numbers-master".dir   = "master";
-  inputs."unicode_numbers-master".owner = "nim-nix-pkgs";
-  inputs."unicode_numbers-master".ref   = "master";
-  inputs."unicode_numbers-master".repo  = "unicode_numbers";
-  inputs."unicode_numbers-master".type  = "github";
-  inputs."unicode_numbers-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."unicode_numbers-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
